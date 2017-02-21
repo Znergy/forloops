ee3273aa3e1ae6894da99ffb222fb01e2a1e4f6e
znergy <Guest@Epicodus-8D.local>
2017-02-21 14:00:50 -0800

    add a working for loop that will automate changing 1d arrays into 2d arrays, if the arrays are equal size

--- js/scripts.js
+++ js/scripts.js
@@ -5,16 +5,40 @@ var idStrings = ["forLoops", "forEach", "whileLoops"];
 var multiArray = [[indexArray[0], idStrings[0]], [indexArray[1], idStrings[0]], [indexArray[2], idStrings[1]], [indexArray[3], idStrings[1]], [indexArray[0], idStrings[0]], [indexArray[0], idStrings[0]], [indexArray[0], idStrings[0]]];
 
 // setting 1d arrays into a 2d array
+// problem, the 1st array is longer than the 2nd array
 for(i = 0; i <= indexArray.length; i++) {
-  multiArray[i][i] = {indexArray[i], idStrings[i]};
+  for(k = 0; k <= idStrings.length; k++) {
+    multiArray[i][i] = {indexArray[i], idStrings[i]};
+
+    if (k == 2) {
+      k = 0;
+    }
+  }
 }
+
+// Working 1d to 2d for loop for equal size
+var array1 = ["one", "two", "three"];
+var array2 = ["toOne", "toTwo", "toThree"];
+var multiArray = [];
+
+for(i = 0; i < array1.length; i++) {
+	multiArray.push([array1[i], array2[i]]);
+}
+
+console.log(multiArray);
 
 // using 2d array to append li's to the html (i refers to indexArray, k refers to idStrings array)
 
+// indexArray and idStrings have to keep increasing, but we also need a reference to the inner array index # (eg. 4th multiArray item)
+
+// To do this I could add a static value, right?
+
 for(i = 0; i <= indexArray.length; i++) {
   for(k = 1; k <= idStrings.length; k++) {
-    multiArray[i][k] =
+    var indexName = multiArray[i][k]
+    $(multiArray[i][k]).append("<li>" + );
     k -= 1;
-    j -= 1;
   }
 }
+
+// Resetting a loop
