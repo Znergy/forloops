c90fb623a5e97319fbbe65a53c9a841efdab56f6
znergy <Guest@Epicodus-8D.local>
2017-02-21 14:30:40 -0800

    add a for loop to assign two 1d arrays to a 2d array

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,26 @@
+// This will take an item from a smaller array and apply each item to every item of the bigger array (first -> forloops, first -> foreach, first-> whileloops, second -> forloops, etc.)
 var indexArray = ["First Item In Array", "Second Item In Array", "Third Item In Array", "Fourth Item In Array", "Fifth Item In Array", "Sixth Item In Array", "Seventh Item In Array", "Eighth Item In Array", "Ninth Item In Array"];
-
 var idStrings = ["forLoops", "forEach", "whileLoops"];
+var multiArray = [];
+var count = 0;
 
-var multiArray = [[indexArray[0], idStrings[0]], [indexArray[1], idStrings[0]], [indexArray[2], idStrings[1]], [indexArray[3], idStrings[1]], [indexArray[0], idStrings[0]], [indexArray[0], idStrings[0]], [indexArray[0], idStrings[0]]];
+// if count gets to 2 then reset array2
+for(i = 0; i < indexArray.length; i++) {
+	for(k = 0; k < idStrings.length; k++) {
+    multiArray.push([indexArray[i], idStrings[k]]);
+    count++; // increases each loop
 
-// setting 1d arrays into a 2d array
-// problem, the 1st array is longer than the 2nd array
-for(i = 0; i <= indexArray.length; i++) {
-  for(k = 0; k <= idStrings.length; k++) {
-    multiArray[i][i] = {indexArray[i], idStrings[i]};
-
-    if (k == 2) {
-      k = 0;
-    }
+    // this will run when the loop has cycled..
+    // through every item in the second array
+    if (count === indexArray.length) {
+      // this will run when 1st for loop has looped equal to 1st array
+      if (i == idStrings.length) {
+        k = 0; // reset 2nd for loop
+    	}
+  	}
   }
 }
+console.log(multiArray);
 
 // Working 1d to 2d for loop for equal size
 var array1 = ["one", "two", "three"];
@@ -24,17 +30,14 @@ var multiArray = [];
 for(i = 0; i < array1.length; i++) {
 	multiArray.push([array1[i], array2[i]]);
 }
-
 console.log(multiArray);
 
-// using 2d array to append li's to the html (i refers to indexArray, k refers to idStrings array)
 
-// indexArray and idStrings have to keep increasing, but we also need a reference to the inner array index # (eg. 4th multiArray item)
-
-// To do this I could add a static value, right?
-
-for(i = 0; i <= indexArray.length; i++) {
-  for(k = 1; k <= idStrings.length; k++) {
+// this will take a 2d array and use the data in a meaningful way
+// it will use the inner array index 1 to target the html id
+// and it will use the inner array index 0 to add a message
+for(i = 0; i < indexArray.length; i++) {
+  for(k = 1; k < idStrings.length; k++) {
     var indexName = multiArray[i][k]
     $(multiArray[i][k]).append("<li>" + );
     k -= 1;
